05725efeccc384c0117b40f5d7b107455c990246
HCVelez <852@holbertonschool.com>
2020-02-05 16:06:06 +0000

    C program to print a string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#import <stdio.h>
+
+/**
+ * main - print a string using printf
+ *
+ * Return: always 0
+/*
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
