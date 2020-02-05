07c6510c2ab4d379861a433859c2fac718587d19
HCVelez <852@holbertonschool.com>
2020-02-05 18:32:52 +0000

    program to write a string to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - program to print a sting to standard error
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	int len;
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	for (len = 0; str[len] != \n; len++)
+		;
+	len++;
+	write(1, str, len);
+	return (1);
+}
