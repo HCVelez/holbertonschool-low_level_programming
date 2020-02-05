501f7e7099149a7ed1636f8ab756a6a0fa354758
HCVelez <852@holbertonschool.com>
2020-02-05 18:36:28 +0000

    put single quotes on newline char in for loop

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,14 @@
  * main - program to print a sting to standard error
  *
  * Return: 1
-*/
+ */
 
 int main(void)
 {
 	int len;
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	for (len = 0; str[len] != \n; len++)
+	for (len = 0; str[len] != '\n'; len++)
 		;
 	len++;
 	write(1, str, len);
