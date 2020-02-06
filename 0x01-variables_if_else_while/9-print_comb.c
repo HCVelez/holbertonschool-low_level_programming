ff2d57699c3f960e017ce787efc614a2dfaeb243
HCVelez <852@holbertonschool.com>
2020-02-06 18:13:55 +0000

    program to print single digit nums seperated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers seperated by comma
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
