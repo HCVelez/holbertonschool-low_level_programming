50276e0dcad40ec0ba91a4c96fd5cc319b80bee8
HCVelez <852@holbertonschool.com>
2020-02-07 19:05:46 +0000

    program to print 2 pairs of double digit combonation

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print 2 2 digit number combos seperated by commas
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int li = 0;
+	int ri = 0;
+
+	while (li < 99)
+	{
+		while (ri < 100)
+		{
+			putchar((li / 10) + '0');
+			putchar((li % 10) + '0');
+			putchar(' ');
+			putchar((ri / 10) + '0');
+			putchar((ri % 10) + '0');
+
+			if (li != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			ri++;
+		}
+		li++;
+		ri = li + 1;
+	}
+	putchar('\n');
+	return (0);
+}
