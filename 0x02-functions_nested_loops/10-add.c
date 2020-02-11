695d5ca091ee63b5428233c6fa4edfc12615256d
HCVelez <852@holbertonschool.com>
2020-02-11 19:11:31 +0000

    program to add to integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - program to add 2 integers
+ *
+ * @a: number to add
+ * @b: number to add
+ * Return: solution
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
