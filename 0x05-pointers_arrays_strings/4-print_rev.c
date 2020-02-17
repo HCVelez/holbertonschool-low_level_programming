70059936fdbe517fd00c183122635bac3897fed7
HCVelez <852@holbertonschool.com>
2020-02-17 17:31:27 +0000

    function ro print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+k#include "holberton.h"
+
+/**
+ * print_rev - print a string in reverse
+ *
+ * @s: string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len++] != '\0')
+	;
+	len--;
+
+	while (len >= 0)
+	{
+		_putchar(s[--len]);
+	}
+	_putchar('\n');
+}
