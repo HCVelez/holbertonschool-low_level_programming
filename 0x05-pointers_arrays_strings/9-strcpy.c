8c6d96a2acae1f891c1c643fbba90c6c449b3e12
HCVelez <852@holbertonschool.com>
2020-02-18 19:29:44 +0000

    function to copy a string to a new destination in memory

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - function to copy a string
+ *
+ * @dest: new string
+ * @src: string to copy
+ * Return: pointer to new string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		dest[len] = src[len];
+		len++;
+	}
+
+	return (dest);
+}
