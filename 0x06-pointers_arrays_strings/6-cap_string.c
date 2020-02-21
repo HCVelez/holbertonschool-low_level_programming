5e0a382e2fc0518ac05b33b858c52bb1172150e1
HCVelez <852@holbertonschool.com>
2020-02-21 04:49:21 +0000

    function to capitalize every letter of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,72 @@
+#include "holberton.h"
+
+/**
+ * is_letter - function to test if given char is a letter
+ *
+ * @s: char to test
+ * Return: 1 if letter 0 if not
+ */
+
+int is_letter(char *s)
+{
+	if (*s >= 'a' && *s <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
+/**
+ * is_separator - function to test if given char is a separator
+ *
+ * @s: char to test
+ * Return: 1 if separator, 0 if not
+ */
+
+int is_separator(char *s)
+{
+	switch (*s)
+	{
+		case ' ':
+		case ';':
+		case ',':
+		case '.':
+		case '?':
+		case '!':
+		case '"':
+		case '\t':
+		case '\n':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			return (1);
+		default:
+			return (0);
+	}
+}
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ *
+ * @s: string to capitalize
+ * Return: pointer to the edited string
+ */
+
+char *cap_string(char *s)
+{
+	char *tmp = s;
+	int islet = is_letter(tmp);
+	int sep = is_separator(tmp - 1);
+
+	while (*tmp)
+	{
+		if (islet == 1 && sep == 1)
+		{
+			*tmp = (*tmp + 32);
+		}
+		tmp++;
+	}
+	return (s);
+}
