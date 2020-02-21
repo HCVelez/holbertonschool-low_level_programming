6809ac37500843b5ce7f4fae512074ba87365515
HCVelez <852@holbertonschool.com>
2020-02-21 19:26:33 +0000

    function to encode a string using rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - function to encode a string using rot13
+ *
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+
+char *rot13(char *s)
+{
+	char *arr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *enc = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *beg = s;
+	int index;
+
+	while (*beg)
+	{
+		for (index = 0; arr[index]; index++)
+		{
+			if (arr[index] == *beg)
+			{
+				*beg = enc[index];
+				break;
+			}
+		}
+		beg++;
+ 	}
+	return (s);
+}
