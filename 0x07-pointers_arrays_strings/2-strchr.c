95a3390fa7a5c96b0948e7d756e323cce00e43e7
HCVelez <852@holbertonschool.com>
2020-02-24 16:24:02 +0000

    function to find a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function to locate a char in a string
+ *
+ * @s: string parse
+ * @c: char to find
+ * Return: pointer to first occurance of var c
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *find = s;
+
+	while (*find) 
+	{
+		if (*find == c)
+			return (find);
+		else
+			find++;
+	}
+	return (0);
+}
