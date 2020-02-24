01e3496196561a0ad0468846551f4cf8542cc0ea
HCVelez <852@holbertonschool.com>
2020-02-24 20:16:08 +0000

    function to locate a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _strstr - function to locate a substring
+ *
+ * @haystack: string to parse
+ * @needle: substring to find
+ * Return: pointer to begining of the substring or NULL if nothing is found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int check = 0;
+	char *trav = haystack;
+	char *hold;
+
+	while (*trav++)
+	{
+		if (*trav == needle[check])
+		{
+			hold = trav + 1;
+			while (needle[check])
+			{
+				if (*trav != needle[check])
+				{
+					check = 0;
+					trav = hold;
+					break;
+				}
+				else if (needle[check])
+					return (trav);
+				trav++;
+			}
+		}
+	}
+	return (0);
+}
