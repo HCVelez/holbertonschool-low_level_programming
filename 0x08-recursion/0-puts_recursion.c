ce63f8dda96714e9d61a3d0c91d017042f6881a2
HCVelez <852@holbertonschool.com>
2020-02-26 17:13:20 +0000

    betty compliance

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _puts_recersion - function to print a string
+ * _puts_recursion - function to print a string
  *
  * @s: string to print
  * Return: void
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		_putchar('\n');
-	else 
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
