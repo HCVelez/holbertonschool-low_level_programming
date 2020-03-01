64810812553f32b49a97410dcf46aea2be8a073b
HCVelez <852@holbertonschool.com>
2020-03-01 01:02:37 +0000

    program to print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints its name
+ *
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0];
+
+	return (0);
+}
