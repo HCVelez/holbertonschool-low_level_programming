cb6b2954d587c042afa0bf4edd5c523d4f617131
HCVelez <852@holbertonschool.com>
2020-03-03 21:37:17 +0000

    function to copy a string to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that copies a string to a newly alloced space in memory
+ *
+ * @str: string to copy
+ * Return: dest if successful, 0/NULL if not
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int index = 0, len = 0;
+	char *dest;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len++])
+	;
+
+	dest = malloc(sizeof(char) * len + 1);
+
+	if (!dest)
+		return (0);
+
+	while (str[index])
+	{
+		dest[index] = str[index];
+		index++;
+	}
+
+	if (dest[index] == '\0')
+		return (dest);
+
+	return (0);
+}
