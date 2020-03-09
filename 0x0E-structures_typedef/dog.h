b4e05843a44dba9c9b48e25e094102567500430f
HCVelez <852@holbertonschool.com>
2020-03-09 21:13:17 +0000

    project header file including struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct to keep track of basic dog info
+ *
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
