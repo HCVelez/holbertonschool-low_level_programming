4caed6e0ecc802b3506e729b74c033c402f27a94
HCVelez <852@holbertonschool.com>
2020-03-13 16:53:44 +0000

    added std header files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #define VARIADIC_FUNCTIONS_H
 #ifndef VARIADIC_FUNTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
