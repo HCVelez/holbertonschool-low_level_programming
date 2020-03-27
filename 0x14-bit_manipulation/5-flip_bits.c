76f44c938d724c5198d67a0cf53c92e327558b2d
HCVelez <852@holbertonschool.com>
2020-03-27 17:57:18 +0000

    function to return number of bits needed to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - function to return the number of bits you need to flip
+ * @n:1st number
+ * @m: 2nd number
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int cmp;
+	unsigned int flip = 0;
+
+	cmp = n ^ m;
+
+	while (cmp != 0)
+	{
+		if ((1 & cmp) > 0)
+			flip++;
+		cmp >>= 1;
+	}
+
+	return (flip);
+}
