28fe82915713468648f3fd6bfc6d64d7d74b4ca8
HCVelez <852@holbertonschool.com>
2020-03-31 18:59:09 +0000

    changed test for wrcheck to -1 from len

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		wrcheck = write(nfcheck, text_content, len);
 	}
 	close(nfcheck);
-	if (wrcheck != len)
+	if (wrcheck == -1)
 		return (-1);
 
 	return (1);
